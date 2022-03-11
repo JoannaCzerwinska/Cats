04c9e536bb18751f6aa661806d635012cf2b01e5
Joanna Czerwinska <joanna.czerwinska@sainsburys.co.uk>
2022-03-11 15:51:03 +0000

    Added lazy initalisation

--- app/src/main/java/com/example/cats/common/composition/AppCompositionRoot.kt
+++ app/src/main/java/com/example/cats/common/composition/AppCompositionRoot.kt
@@ -1,19 +1,27 @@
 package com.example.cats.common.composition
 
+import androidx.annotation.UiThread
 import com.example.cats.Constants
 import com.example.cats.api.IApiService
 import com.example.cats.breeds.FetchBreedsUseCase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+@UiThread
 class AppCompositionRoot {
 
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(Constants.BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    // "by lazy" - instantiated object will be cashed and then re-used on subsequent invocations
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 
-    private val apiService: IApiService = retrofit.create(IApiService::class.java)
+    private val apiService: IApiService by lazy {
+        retrofit.create(IApiService::class.java)
+    }
 
+    // get() always gets a new instance of the use case
     val fetchBreedsUseCase get() = FetchBreedsUseCase(apiService)
 }
